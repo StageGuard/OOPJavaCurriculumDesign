9ca6057dbfa9a2317f321ecd6891cbe22a6ee96c
StageGuard <1355416608@qq.com>
2021-06-15 23:43:09 +0800

    backend: multi thread via thread pool

--- backend/src/main/java/me/stageguard/oopcd/backend/ApplicationMain.java
+++ backend/src/main/java/me/stageguard/oopcd/backend/ApplicationMain.java
@@ -2,10 +2,12 @@ package me.stageguard.oopcd.backend;
 
 import me.stageguard.oopcd.backend.netty.NettyHttpServer.NettyHttpServerBuilder;
 import me.stageguard.oopcd.backend.database.Database.DatabaseBuilder;
+import me.stageguard.oopcd.backend.netty.handler.TestExecuteQueryHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -17,9 +19,9 @@ public class ApplicationMain {
 
     public static void main(String[] args) {
         var nettyHttpService = NettyHttpServerBuilder.create(8081)
-            .route(new ArrayList<>(
-
-            ))
+            .route(new ArrayList<>(Arrays.asList(
+                    new TestExecuteQueryHandler()
+            )))
         .build();
         executors.execute(nettyHttpService);
         var databaseService = DatabaseBuilder.create("localhost", 3306)
