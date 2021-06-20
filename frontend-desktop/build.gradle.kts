7d07eb30729cb8eccace44f7bbbd65433ed20a55
StageGuard <1355416608@qq.com>
2021-06-20 21:23:26 +0800

    frontend-desktop: init project

---
+++ frontend-desktop/build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.compose") version "0.4.0"
+}
+
+
+repositories {
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(compose.desktop.currentOs)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
+compose.desktop {
+    application {
+        mainClass = "me.stageguard.oopcd.frontend.desktop.DesktopMainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Msi)
+            packageName = "OOPCDFrontendDesktop"
+            packageVersion = "1.0.0"
+        }
+    }
+}
